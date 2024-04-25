e2df0169b6443331b0c8cf38d4e86178da1be311
araque1990 <araque1990.cdap@gmail.com>
2024-04-25 16:11:33 +0200

    This is a commit is for you Mark

--- PromoAdvanced/Model/ResourceModel/PromoAdvancedEntity.php
+++ PromoAdvanced/Model/ResourceModel/PromoAdvancedEntity.php
@@ -12,35 +12,37 @@ use Magento\Store\Model\StoreManagerInterface;
 
 class PromoAdvancedEntity extends AbstractEntity
 {
-  const MAIN_TABLE = 'promo_advanced_entity';
-  const ID_FIELD_NAME = 'entity_id';
+    const MAIN_TABLE = 'promo_advanced_entity';
+    const ID_FIELD_NAME = 'entity_id';
 
 
-  public function __construct(
-    Context $context,
-    protected StoreManagerInterface $storeManager,
-    protected ManagerInterface $eventManager,
-    array $data = []
-  ) {
-    parent::__construct($context, $data);
-    $this->setType(ModelPromoAdvancedEntity::ENTITY);
-    $this->setConnection(ModelPromoAdvancedEntity::ENTITY . '_read', ModelPromoAdvancedEntity::ENTITY . '_write');
-  }
-
-  /**
-   * Return Entity Type instance
-   *
-   * @return \Magento\Eav\Model\Entity\Type
-   */
-  public function getEntityType()
-  {
-    if (empty($this->_type)) {-
-      $this->setType(ModelPromoAdvancedEntity::ENTITY);
+    public function __construct(
+        Context                         $context,
+        protected StoreManagerInterface $storeManager,
+        protected ManagerInterface      $eventManager,
+        array                           $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->setType(ModelPromoAdvancedEntity::ENTITY);
+        $this->setConnection(ModelPromoAdvancedEntity::ENTITY . '_read', ModelPromoAdvancedEntity::ENTITY . '_write');
     }
-    return parent::getEntityType();
-  }
 
-  protected function _getDefaultAttributes()
+    /**
+     * Return Entity Type instance
+     *
+     * @return \Magento\Eav\Model\Entity\Type
+     */
+    public function getEntityType()
+    {
+        if (empty($this->_type)) {
+            -
+            $this->setType(ModelPromoAdvancedEntity::ENTITY);
+        }
+        return parent::getEntityType();
+    }
+
+    protected function _getDefaultAttributes()
     {
         return [
             'entity_id',
--- PromoAdvanced/Model/ResourceModel/PromoAdvancedEntity/Grid/Collection.php
+++ PromoAdvanced/Model/ResourceModel/PromoAdvancedEntity/Grid/Collection.php
@@ -4,16 +4,31 @@ declare(strict_types=1);
 
 namespace Caraque\PromoAdvanced\Model\ResourceModel\PromoAdvancedEntity\Grid;
 
-use Caraque\PromoAdvanced\Model\PromoAdvancedRepository;
 use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
 
 class Collection extends SearchResult
 {
+    /**
+     * @return void
+     */
     protected function _construct(): void
     {
         $this->_init(
             'Caraque\PromoAdvanced\Model\PromoAdvancedEntity',
             'Caraque\PromoAdvanced\Model\ResourceModel\PromoAdvancedEntity'
         );
+    }
+
+    /**
+     * @return $this
+     */
+    protected function _initSelect(): Collection
+    {
+        parent::_initSelect();
+
+        $this->addFieldToSelect('name');
+        $this->addFieldToSelect('store_id');
+
+        return $this;
     }
 }
