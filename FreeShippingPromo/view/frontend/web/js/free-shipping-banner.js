92e7720acaf0df72d4df22e1b656f5cd689e1133
araque1990 <araque1990.cdap@gmail.com>
2024-06-11 16:00:15 +0200

    This was an example about how to use KnockoutJs, the next step is create a functionality applying a logic for the shopping cart

--- FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -1,18 +1,37 @@
-define(["uiComponent"], function (Component) {
+define([
+    "uiComponent",
+    "Magento_Customer/js/customer-data"
+    // "ko"
+], function (
+    Component,
+    customerData
+    //ko
+) {
     "use strict";
 
     return Component.extend({
         defaults: {
+            //subtotal: ko.observable(33.00),
             subtotal: 33.00,
-            template: 'Caraque_FreeShippingPromo/free-shipping-banner'
+            template: 'Caraque_FreeShippingPromo/free-shipping-banner',
+            tracks: {
+                subtotal: true
+            }
         },
         initialize: function () {
             this._super();
+            var self = this;
+
+            window.setTimeout(function () {
+                // self.subtotal(35.00);
+                self.subtotal = 35.00;
+            }, 2000);
 
             console.log(this.message);
             console.log(this.message_xml);
         },
         formatCurrency: function (value) {
+            // return '$' + value().toFixed(2);
             return '$' + value.toFixed(2);
         }
     });
