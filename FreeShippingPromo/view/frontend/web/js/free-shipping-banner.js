694d3e83837cd5ea7a5aefa29ee95bbba8732923
araque1990 <araque1990.cdap@gmail.com>
2024-06-12 12:07:54 +0200

    I've created a functionality that let the admin to configure freeshipping promo just indicate the amount that the admin want to gift the shipping

--- FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -1,10 +1,14 @@
 define([
     "uiComponent",
-    "Magento_Customer/js/customer-data"
+    "Magento_Customer/js/customer-data",
+    'underscore',
+    'knockout'
     // "ko"
 ], function (
     Component,
-    customerData
+    customerData,
+    _,
+    ko
     //ko
 ) {
     "use strict";
@@ -12,23 +16,44 @@ define([
     return Component.extend({
         defaults: {
             //subtotal: ko.observable(33.00),
-            subtotal: 33.00,
+            freeShippingThreshold: 100,
+            subtotal: 0.00,
             template: 'Caraque_FreeShippingPromo/free-shipping-banner',
             tracks: {
-                subtotal: true
+                subtotal: true,
             }
         },
         initialize: function () {
             this._super();
             var self = this;
+            var cart = customerData.get('cart');
+            customerData.getInitCustomerData().done(function () {
+                if (!_.isEmpty(cart()) && !_.isUndefined(cart().subtotalAmount)) {
+                    self.subtotal = parseFloat(cart().subtotalAmount);
+                }
+            });
 
-            window.setTimeout(function () {
-                // self.subtotal(35.00);
-                self.subtotal = 35.00;
-            }, 2000);
-
-            console.log(this.message);
-            console.log(this.message_xml);
+            cart.subscribe(function (cart) {
+                if (!_.isEmpty(cart) && !_.isUndefined(cart.subtotalAmount)) {
+                    self.subtotal = parseFloat(cart.subtotalAmount);
+                }
+            });
+            self.message = ko.computed(function () {
+                // subtotal = 0, return messageDefault
+                if (_.isUndefined(self.subtotal) || self.subtotal === 0) {
+                    return self.messageDefault;
+                }
+                // subtotal >0 and < 100, return messageItemsInCart
+                else if (self.subtotal > 0 && self.subtotal < self.freeShippingThreshold) {
+                    let subtotalRemaining = self.freeShippingThreshold - self.subtotal;
+                    let formatSubtotalRemaining = self.formatCurrency(subtotalRemaining);
+                    return self.messageItemsInCart.replace('$XX.XX', formatSubtotalRemaining);
+                }
+                // subtotal >= 100, return messageFreeShipping
+                if (self.subtotal >= self.freeShippingThreshold) {
+                    return self.messageFreeShipping;
+                }
+            });
         },
         formatCurrency: function (value) {
             // return '$' + value().toFixed(2);
